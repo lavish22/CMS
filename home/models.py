6f512cf2b32f1554dd492d27a2307076034d1c6c
Shahzeb04 <mohd.shahzeb@collegedunia.com>
2020-02-07 10:00:56 +0530

    till lesson15, updated django and wagtail

--- home/models.py
+++ home/models.py
@@ -1,10 +1,29 @@
 from django.db import models
 
-from wagtail.core.models import Page
-from wagtail.admin.edit_handlers import FieldPanel,PageChooserPanel,StreamFieldPanel
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
+from wagtail.admin.edit_handlers import FieldPanel,PageChooserPanel,StreamFieldPanel, InlinePanel, MultiFieldPanel
 from wagtail.core.fields import RichTextField,StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 from streams import blocks
+
+class HomePageCarouselImages(Orderable):
+    '''
+    between 1 and 5 images for home page carousel
+    '''
+
+    page = ParentalKey("home.HomePage", related_name = "carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+"
+    )
+    panels = [
+        ImageChooserPanel("carousel_image")
+    ]
 
 class HomePage(Page):
     '''
@@ -29,11 +48,20 @@ class HomePage(Page):
         related_name="+"
     )
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
-        StreamFieldPanel("content")
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_subtitle"),
+                ImageChooserPanel("banner_image"),
+                PageChooserPanel("banner_cta"),
+            ], heading = "Banner Options"
+        ),
+        StreamFieldPanel("content"),
+        MultiFieldPanel(
+            [
+                InlinePanel("carousel_images", max_num=5, min_num=1, label="Image"),        
+            ], heading = "Carousel Images"
+        )
     ]
 
     content =  StreamField (
@@ -47,3 +75,4 @@ class HomePage(Page):
     class Meta:
         verbose_name = "Page"
         verbose_name_plural = "Pages"
+
