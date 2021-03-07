c3d4199aeea1ed4471a860f1d75deef8330bb858
localit <localit@LTB0219474-MAC.lan>
2021-03-07 20:46:58 +0530

    forms page slight modify

--- contact/models.py
+++ contact/models.py
@@ -13,6 +13,7 @@ from wagtail.admin.edit_handlers import (
 from wagtail.core.fields import RichTextField
 from wagtail.contrib.forms.models import AbstractFormField, AbstractEmailForm
 from wagtailcaptcha.models import WagtailCaptchaEmailForm
+from wagtail.contrib.forms.views import SubmissionsListView
 
 
 class FormField(AbstractFormField):
@@ -21,6 +22,17 @@ class FormField(AbstractFormField):
         on_delete=models.CASCADE,
         related_name='form_fields',
     )
+
+class CustomSubmissionsListView(SubmissionsListView):
+    paginate_by = 50  # show more submissions per page, default is 20
+    ordering = ('submit_time',)  # order submissions by oldest first, normally newest first
+    ordering_csv = ('-submit_time',)  # order csv export by newest first, normally oldest first
+
+    # override the method to generate csv filename
+    def get_csv_filename(self):
+        """ Returns the filename for CSV file with page slug at start"""
+        filename = super().get_csv_filename()
+        return self.form_page.slug + '-' + filename
 
 
 class ContactPage(WagtailCaptchaEmailForm):
@@ -33,6 +45,9 @@ class ContactPage(WagtailCaptchaEmailForm):
     # This is the default path.
     # If ignored, Wagtail adds _landing.html to your template name
     landing_page_template = "contact/contact_page_landing.html"
+
+    # set custom view class as class attribute
+    submissions_list_view_class = CustomSubmissionsListView
 
     intro = RichTextField(blank=True)
     thank_you_text = RichTextField(blank=True)
